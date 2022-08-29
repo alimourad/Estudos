df455f98b1f1f3b23c189fb4ccd771f915e55440
alimourad <ali.jafar.rhamza@gmail.com>
2022-08-29 10:16:18 -0300

    Correção da lista de tarefas

--- Novos Estudos 2022/Lista de Tarefas com LocalStorage/script.js
+++ Novos Estudos 2022/Lista de Tarefas com LocalStorage/script.js
@@ -1,10 +1,22 @@
-var listaDeItens = ""
-
 function setLista(){
+    //ler o valor escrito no input
     var novoItem = document.getElementById("input").value;
+    if (novoItem == "") throw 'Campo vazio';
+
+    //puxar os itens salvos anteriormente
+    var listaDeItens = localStorage.getItem("lista");
+
+    //se a lista for nula, retornar string vazio. Evitar que imprima null na tela.
+    listaDeItens = listaDeItens == null ? "" : listaDeItens;
+
+    //adicionar o valor do input na variavel e gravar
     listaDeItens += `<li>${novoItem}</li>`;
     localStorage.setItem("lista", listaDeItens);
+
+    //mostrar a lista na tela
     getLista();
+
+    //limpar o input
     document.getElementById("input").value = "";
 }
 
