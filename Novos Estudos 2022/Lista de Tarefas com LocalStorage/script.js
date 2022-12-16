127290716e4f23d52113c06079c25b3718f46ed1
alimourad <ali.jafar.rhamza@gmail.com>
2022-12-15 21:12:26 -0300

    commit testes

--- Novos Estudos 2022/Lista de Tarefas com LocalStorage/script.js
+++ Novos Estudos 2022/Lista de Tarefas com LocalStorage/script.js
@@ -1,7 +1,14 @@
-var listaDeItens = ""
-
 function setLista(){
+    //ler o valor escrito no input
     var novoItem = document.getElementById("input").value;
+    if (novoItem == "") throw 'Campo vazio';
+
+    //puxar os itens salvos anteriormente
+    var listaDeItens = localStorage.getItem("lista");
+
+    //se a lista for nula, retornar string vazio. Evitar que imprima null na tela.
+    listaDeItens = listaDeItens == null ? "" : listaDeItens;
+
     listaDeItens += `<li>${novoItem}</li>`;
     localStorage.setItem("lista", listaDeItens);
     getLista();
